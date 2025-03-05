38bfd7078810e6c4b7e4c9feff32101a1f08277e
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-05 14:03:39 +0530

    [sujoy]desc: Toggle the case of each character in a string

---
+++ ToggleCase.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ToggleCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        StringBuilder toggled = new StringBuilder();
+
+        for (char ch : input.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                toggled.append(Character.toLowerCase(ch));
+            } else {
+                toggled.append(Character.toUpperCase(ch));
+            }
+        }
+
+        System.out.println("Toggled String: " + toggled);
+    }
+}
