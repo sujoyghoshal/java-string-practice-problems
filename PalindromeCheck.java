8c3406888df204f56c73248d935bc7be21dad32f
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-05 14:01:26 +0530

    [sujoy]desc: Check if a string is a palindrome

---
+++ PalindromeCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome ? "Palindrome" : "Not a Palindrome");
+    }
+}
